d064d5a5972ce997c3ee7df5f96786a86ed13819
kelani34 <kelanitaiwo34@gmail.com>
2023-04-29 20:57:08 +0100

    feat: get current user location

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+// get user current location
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+
+function showPosition(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  getWeatherData(lat, lon);
+}
+
+getLocation();
