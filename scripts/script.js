e4fcaaf368c8513e213651ab6f0e90339cdc38ec
kelani34 <kelanitaiwo34@gmail.com>
2023-04-29 21:27:24 +0100

    feat: get current user data based on location

--- scripts/script.js
+++ scripts/script.js
@@ -15,3 +15,31 @@ function showPosition(position) {
 }
 
 getLocation();
+
+async function getWeatherData(lat, lon) {
+  const API_KEY = "8ff2d6e561e8c713a4eddb92e542e175";
+  const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const forecastWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+  try {
+    const currentWeatherResponse = await fetch(currentWeatherUrl);
+    const currentWeatherData = await currentWeatherResponse.json();
+
+    const forecastWeatherResponse = await fetch(forecastWeatherUrl);
+    const forecastWeatherData = await forecastWeatherResponse.json();
+
+    localStorage.setItem(
+      "currentWeatherData",
+      JSON.stringify(currentWeatherData)
+    );
+    localStorage.setItem(
+      "forecastWeatherData",
+      JSON.stringify(forecastWeatherData)
+    );
+
+    displayCurrentWeather(currentWeatherData);
+    displayForecastWeather(forecastWeatherData);
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+  }
+}
